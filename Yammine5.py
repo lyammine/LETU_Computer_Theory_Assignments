a9e1ac5f25e4b6b96b4ac7811a89a65cd5175ced
Leila Yammine <leilayammine@gmail.com>
2014-03-03 01:05:05 -0600

    Assignments #0 and #5
    Pushing:
    
    Assignment #0 - RegEx Character Checker
    Assignment #5 - Turing Machine Simulator

---
+++ Yammine5.py
@@ -0,0 +1,30 @@
+  ##################################################################
+#  Student name: Leila Yammine
+#  Course: COSC 4603 - Computer Theory
+#  Assignment: #5 - Turing Machine Simulator
+#  File name: Yammine5.py
+#  Program Purpose: To create a Turing machine simulator that accepts as input the description of a Turing machine, the initial tape configuration (machine input). The simulator will process tape input, and any output will be on the tape at the end of the computation. 
+#
+#  Program Limitations: None known
+#  Development Computer: Macbook Pro
+#  Operating System: OS X Mavericks
+#  Integrated Development Environment (IDE): None (Terminal)
+#  Program's Operational Status: 
+##################################################################
+
+import sys
+
+def main():
+
+def currentState():
+
+def inputSymbol():
+
+def newState():
+
+def writeSymbol():
+
+def moveDirection():
+
+if __name__ == '__main__':
+    main()
